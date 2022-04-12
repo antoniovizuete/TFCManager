95bfedf6f463128fe8eb99aa84390c98535f0ac7
Pedro Calvo <pedrojcalvo@gmail.com>
2022-04-12 21:05:37 +0200

    Feat:added getUser services

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { getUsers } from './services/user.services';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect( () =>{
+    const getAllUsers = async() => {
+      setUsers(await getUsers());
+    }
+    getAllUsers();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {users.map(user=> <p>id: {user._id}, nombre: {user.name}</p>)}
         <a
           className="App-link"
           href="https://reactjs.org"
